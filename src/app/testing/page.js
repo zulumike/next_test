83c023a6e67b533e4922c9bf94008e9bec7045a2
zulumike <ole@zmmaskin.no>
2023-07-28 09:14:33 +0200

    Initial

---
+++ src/app/testing/page.js
@@ -0,0 +1,17 @@
+
+export default function testing() {
+    return (
+        <div>
+            <input
+                type="text"
+                id="idfirst"
+                name="firstname"
+            />
+            <input
+                type="text"
+                id="idlastname4"
+                name="lastname4"
+            />
+        </div>
+    )
+}
